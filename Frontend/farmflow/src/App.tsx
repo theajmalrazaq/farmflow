f1d82184f6220d6520280f8689f7f318819a2ffa
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2026-05-06 00:46:39 +0500

    added

---
+++ Frontend/farmflow/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1 className='text-red-600 text-3xl'>Ajmal Razaq</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
